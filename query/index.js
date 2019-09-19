9b6e94db45a496f5177311e8535464316591b6aa
Eunwoo <dmsdn960@gmail.com>
2019-09-20 01:17:10 +0900

    complete prettier setting

--- query/index.js
+++ query/index.js
@@ -1,14 +1,14 @@
 import gql from 'graphql-tag';
 
-const GET_PLACES = gql `
-query getPlaces($page:Int) {
-  getPlaces(page: $page) {
-    id
-    name
+const GET_PLACES = gql`
+  query getPlaces($page: Int) {
+    getPlaces(page: $page) {
+      id
+      name
+    }
   }
-}
 `;
 
 export default {
-    GET_PLACES,
+  GET_PLACES,
 };
