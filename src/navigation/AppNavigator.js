bc02792257c56ed501b09d24fc7e707754ae4e69
Eunwoo <dmsdn960@gmail.com>
2019-09-20 02:39:56 +0900

    add auth screen

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,10 +1,12 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import AuthLoadingScreen from '../screens/AuthLoadingScreen';
+import AuthNavigator from './AuthNavigator';
 import MainTabNavigator from './MainTabNavigator';
 
 export default createAppContainer(
   createSwitchNavigator({
     AuthLoading: AuthLoadingScreen,
+    Auth: AuthNavigator,
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
     Main: MainTabNavigator,
---
+++ src/screens/SignInScreen.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { AsyncStorage, StyleSheet, View, Button, Text } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
+export default function SignInScreen({ navigation }) {
+  async function signInAsync() {
+    await AsyncStorage.setItem('userToken', 'abc');
+    navigation.navigate('App');
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>로그인화면</Text>
+      <Button title="Sign in!" onPress={signInAsync} />
+    </View>
+  );
+}
