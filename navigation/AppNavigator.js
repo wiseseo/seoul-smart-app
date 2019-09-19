aef0da998c6285b56902398ec0ce2d21e6349b6a
Eunwoo <dmsdn960@gmail.com>
2019-09-20 01:42:57 +0900

    add auth loading screen check async storage

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,10 +1,10 @@
-import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-
+import AuthLoadingScreen from '../screens/AuthLoadingScreen';
 import MainTabNavigator from './MainTabNavigator';
 
 export default createAppContainer(
   createSwitchNavigator({
+    AuthLoading: AuthLoadingScreen,
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
     Main: MainTabNavigator,
---
+++ screens/AuthLoadingScreen.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+import {
+  ActivityIndicator,
+  AsyncStorage,
+  StatusBar,
+  StyleSheet,
+  View,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
+export default function AuthLoadingScreen(props) {
+  // Fetch the token from storage then navigate to our appropriate place
+  async function bootstrapAsync() {
+    const userToken = await AsyncStorage.getItem('userToken');
+
+    // This will switch to the App screen or Auth screen and this loading
+    // screen will be unmounted and thrown away.
+    // eslint-disable-next-line react/prop-types
+    props.navigation.navigate(userToken ? 'App' : 'Auth');
+  }
+
+  useEffect(() => {
+    bootstrapAsync();
+  }, []);
+
+  // Render any loading content that you like here
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator />
+      <StatusBar barStyle="default" />
+    </View>
+  );
+}
