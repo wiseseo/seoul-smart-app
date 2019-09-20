542a2b598a1ab135fa7d03fbe9af86b5c351f850
Eunwoo <dmsdn960@gmail.com>
2019-09-21 01:07:38 +0900

    add Activity screen

---
+++ src/screens/ActivityScreen.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+});
+
+export default function ActivityScreen({ navigation }) {
+  /**
+   * Go ahead and delete ExpoConfigView and replace it with your content;
+   * we just wanted to give you a quick view of your config.
+   */
+  return (
+    <View style={styles.container}>
+      <Text>활동페이지</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('Open')}>
+        <Text>개설하기</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+ActivityScreen.navigationOptions = {
+  title: '활동',
+};
