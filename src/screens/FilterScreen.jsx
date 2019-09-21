2988fa44576f8b3bfa734d2092ff1dfb3d440c9b
Eunwoo <dmsdn960@gmail.com>
2019-09-21 20:40:12 +0900

    write filter list

--- src/screens/FilterScreen.jsx
+++ src/screens/FilterScreen.jsx
@@ -11,9 +11,48 @@ const styles = StyleSheet.create({
 });
 
 export default function FilterScreen({ navigation }) {
+  const filter = {
+    facility: [
+      '오픈공간',
+      '회의실',
+      '세미나룸',
+      '녹음실',
+      '부엌',
+      '스터디룸',
+      '연습실',
+      '극장',
+    ],
+    location: [
+      '성북구',
+      '도봉구',
+      '양천구',
+      '금천구',
+      '서대문구',
+      '강동구',
+      '종로구',
+      '동작구',
+      '송파구',
+      '성동구',
+      '마포구',
+      '중구',
+      '은평구',
+      '광진구',
+      '용산구',
+      '동대문구',
+      '중랑구',
+      '노원구',
+      '강서구',
+      '강남구',
+      '강북구',
+      '구로구',
+      '영등포구',
+      '관악구',
+      '서초구',
+    ],
+  };
   return (
     <View style={styles.container}>
-      <Text>{navigation.getParam('type')}</Text>
+      <Text>{filter[navigation.getParam('type')]}</Text>
     </View>
   );
 }
