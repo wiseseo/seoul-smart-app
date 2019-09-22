d862f30c887ad4da84e23cfcccfb9b5629a024c9
Eunwoo <dmsdn960@gmail.com>
2019-09-23 00:38:46 +0900

    add pull on refresh

--- src/components/ActivityList/index.jsx
+++ src/components/ActivityList/index.jsx
@@ -8,9 +8,13 @@ import GET_ACTIVITIES from './query';
 
 export default function ActivityList({ typeFilter, navigate }) {
   const [page, setPage] = useState(2);
-  const { loading, error, data, fetchMore } = useQuery(GET_ACTIVITIES, {
-    variables: { type: typeFilter },
-  });
+  const { loading, error, data, fetchMore, refetch } = useQuery(
+    GET_ACTIVITIES,
+    {
+      variables: { type: typeFilter },
+      // notifyOnNetworkStatusChange: true,
+    }
+  );
 
   useEffect(() => {
     setPage(2);
@@ -18,32 +22,33 @@ export default function ActivityList({ typeFilter, navigate }) {
 
   if (loading) return <Text>로딩</Text>;
   if (error) return <Text>에러</Text>;
+
+  function onEndReached() {
+    setPage(prev => prev + 1);
+    fetchMore({
+      variables: { page, type: typeFilter },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        if (!fetchMoreResult.getActivities.length) return prev;
+        const [{ id }] = fetchMoreResult.getActivities.slice(-1);
+        if (prev.getActivities.filter(activity => activity.id === id).length)
+          return prev;
+        return {
+          getActivities: [
+            ...prev.getActivities,
+            ...fetchMoreResult.getActivities,
+          ],
+        };
+      },
+    });
+  }
   return (
     <FlatList
       data={data.getActivities}
-      initialNumToRender={1}
+      refreshing={loading}
+      onRefresh={() => refetch({ variables: { type: typeFilter } })}
       keyExtractor={({ id }) => id}
       onEndReachedThreshold={1}
-      onEndReached={() => {
-        setPage(prev => prev + 1);
-        fetchMore({
-          variables: { page, type: typeFilter },
-          updateQuery: (prev, { fetchMoreResult }) => {
-            if (!fetchMoreResult.getActivities.length) return prev;
-            const [{ id }] = fetchMoreResult.getActivities.slice(-1);
-            if (
-              prev.getActivities.filter(activity => activity.id === id).length
-            )
-              return prev;
-            return {
-              getActivities: [
-                ...prev.getActivities,
-                ...fetchMoreResult.getActivities,
-              ],
-            };
-          },
-        });
-      }}
+      onEndReached={onEndReached}
       renderItem={({ item: { id, name, type } }) => (
         <Activity id={id} name={name} type={type} navigate={navigate} />
       )}
