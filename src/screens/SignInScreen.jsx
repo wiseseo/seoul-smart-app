cb973b9182ed409373ac7a8983e25a332d0853f2
pongsoyun <thdbstjdud@gmail.com>
2019-09-22 19:57:51 +0900

    naver - success but not found

--- src/screens/SignInScreen.jsx
+++ src/screens/SignInScreen.jsx
@@ -1,16 +1,15 @@
 /* eslint-disable no-undef */
 /* eslint-disable react/prop-types */
 import React, { useState } from 'react';
-import { StyleSheet, View, Button, Text } from 'react-native';
+import { StyleSheet, View, Button, Text, TouchableOpacity } from 'react-native';
 import { AuthSession } from 'expo';
 import axios from 'axios';
-import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const NV_APP_ID = 'UJxRowek7VWI2fNRWzhf';
 const NV_APP_SECRET = 'CedceOMq48';
 const STATE_STRING = 'sfdjlweioj312esdf';
 
-const KK_APP_ID = 'b46cd5f7008567fe15d23d72d4e34040';
+// const KK_APP_ID = 'b46cd5f7008567fe15d23d72d4e34040';
 
 const styles = StyleSheet.create({
   container: {
@@ -19,56 +18,53 @@ const styles = StyleSheet.create({
   },
 });
 
-export function App() {
+export default function SignInScreen({ navigation }) {
   const [token, setToken] = useState();
   const [code, setCode] = useState();
   const [user, setUser] = useState();
-}
 
-async function handleGetAccess() {
-  const {
-    data: { ACCESSTOKEN },
-  } = await axios.get(`https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=${NV_APP_ID}
-  &client_secret=${NV_APP_SECRET}
-  &code=${code}&state=${STATE_STRING}`);
-
-  const config = {
-    headers: {
-      Authorization: `Bearer ${ACCESSTOKEN}`,
-    },
-  };
-
-  setToken(ACCESSTOKEN);
-
-  const { data } = await axios.get(
-    'https://openapi.naver.com/v1/nid/me',
-    config
-  );
-  console.log(data);
-  setUser(data);
-}
-
-async function handlePressAsync() {
-  const redirectUrl = AuthSession.getRedirectUrl();
-  console.log(encodeURIComponent(redirectUrl));
-
-  const result = await AuthSession.startAsync({
-    authUrl: `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NV_APP_ID}&redirect_uri=${encodeURIComponent(
-      redirectUrl
-    )}&state=${STATE_STRING}`,
-  });
-
-  console.log('result : ', result);
-
-  setCode(result.code);
-
-  handleGetAccess();
-}
-
-export default function SignInScreen({ navigation }) {
   async function signInAsync() {
     // await AsyncStorage.setItem('userToken', 'abc'); // 실행시키면 실제로 저장되어서 바로 넘어가짐
     navigation.navigate('Main');
+  }
+
+  async function handleGetAccess() {
+    const {
+      data: { accessToken },
+    } = await axios.get(`https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=${NV_APP_ID}
+    &client_secret=${NV_APP_SECRET}
+    &code=${code}&state=${STATE_STRING}`);
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    };
+    setToken(accessToken);
+
+    const { data } = await axios.get(
+      'https://openapi.naver.com/v1/nid/me',
+      config
+    );
+    console.log(data);
+    setUser(data);
+
+    console.log(apiResult);
+  }
+
+  async function handlePressAsync() {
+    const redirectUrl = AuthSession.getRedirectUrl();
+    console.log(redirectUrl);
+    console.log(encodeURIComponent(redirectUrl));
+
+    const result = await AuthSession.startAsync({
+      authUrl: `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NV_APP_ID}&redirect_uri=${encodeURIComponent(
+        redirectUrl
+      )}&state=${STATE_STRING}`,
+    });
+    console.log("result : ", result);
+    setCode(result.code);
+    handleGetAccess();
   }
 
   return (
