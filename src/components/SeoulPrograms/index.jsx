3102b5b4437cdcd4f33a8b006045b580a6956e83
Eunwoo <dmsdn960@gmail.com>
2019-09-22 14:41:12 +0900

    change programlist map to flatlist

--- src/components/SeoulPrograms/index.jsx
+++ src/components/SeoulPrograms/index.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import { ScrollView, Text, StyleSheet } from 'react-native';
+import { FlatList, Text } from 'react-native';
 import { useQuery } from '@apollo/react-hooks';
 import Program from './Program';
 import GET_PROGRAMS from './query';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default function SeoulPrograms() {
   const { loading, error, data } = useQuery(GET_PROGRAMS);
 
   if (loading) return <Text>로딩</Text>;
   if (error) return <Text>에러</Text>;
-
-  const programs = data.getPrograms.map(({ id, image, title }) => (
-    <Program key={id} uri={image} title={title} />
-  ));
-  return <ScrollView style={styles.container}>{programs}</ScrollView>;
+  return (
+    <FlatList
+      data={data.getPrograms}
+      keyExtractor={({ id }) => id}
+      renderItem={({ item: { image, title } }) => (
+        <Program uri={image} title={title} />
+      )}
+    />
+  );
 }
