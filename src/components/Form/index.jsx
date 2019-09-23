73d061000cc3b0e42780c4ab9c5647fe82a7d38f
Eunwoo <dmsdn960@gmail.com>
2019-09-23 16:57:12 +0900

    add date picker and number input

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -8,7 +8,7 @@ import {
   StyleSheet,
 } from 'react-native';
 import { NavigationActions } from 'react-navigation';
-import PropTypes from 'prop-types';
+import DatePicker from 'react-native-datepicker';
 import TypePicker from '../TypePicker';
 import { useBack } from '../../lib';
 
@@ -23,11 +23,24 @@ const styles = StyleSheet.create({
 export default function Form({ navigate }) {
   const [name, setName] = useState('');
   const [type, setType] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+  const [total, setTotal] = useState('0');
+
   useBack(() => {
     console.log('뒤로가기 버튼');
-
+    console.log(
+      new Date()
+        .toISOString()
+        .split('T')
+        .shift()
+    );
     return true;
   });
+  const current = new Date()
+    .toISOString()
+    .split('T')
+    .shift();
   return (
     <View style={styles.container}>
       <TextInput
@@ -45,12 +58,62 @@ export default function Form({ navigate }) {
               routeName: 'Place',
               params: { selectable: true },
             })
-          )
-        }>
+          )}
+      >
         <Text>장소선택</Text>
       </TouchableOpacity>
+      <DatePicker
+        style={{ width: 200 }}
+        date={date}
+        mode="date"
+        placeholder="날짜 선택"
+        format="YYYY-MM-DD"
+        minDate={current}
+        maxDate="2021-12-01"
+        confirmBtnText="확인"
+        cancelBtnText="취소"
+        customStyles={{
+          dateIcon: {
+            display: 'none',
+          },
+          dateInput: {
+            marginLeft: 36,
+          },
+          // ... You can check the source to find the other keys.
+        }}
+        onDateChange={value => {
+          setDate(value);
+        }}
+      />
+      <DatePicker
+        style={{ width: 200 }}
+        date={time}
+        mode="time"
+        placeholder="시간 선택"
+        format="HH:mm"
+        confirmBtnText="확인"
+        cancelBtnText="취소"
+        customStyles={{
+          dateIcon: {
+            display: 'none',
+          },
+          dateInput: {
+            marginLeft: 36,
+          },
+          // ... You can check the source to find the other keys.
+        }}
+        onDateChange={value => {
+          setTime(value);
+        }}
+      />
+      <TextInput
+        onChangeText={value => {
+          setTotal(value);
+        }}
+        value={total}
+        keyboardType="numeric"
+        placeholder="활동 인원"
+      />
     </View>
   );
 }
-
-Form.propTypes = {};
