52b041abf3dd49f4e652d9d84fa62b4d253707dc
JihyeSeo1120 <mansesjh22@gmail.com>
2019-09-26 22:42:55 +0900

    floating button

--- src/screens/ActivityScreen.jsx
+++ src/screens/ActivityScreen.jsx
@@ -1,9 +1,15 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import SeoulPrograms from '../components/SeoulPrograms';
 import ActivityList from '../components/ActivityList';
 import TypePicker from '../components/TypePicker';
+import { width, height, font } from '../constants/Layout';
+import {
+  NanumGothic,
+  NanumGothicBold,
+  NanumGothicExtraBold,
+} from '../components/StyledText';
 
 const styles = StyleSheet.create({
   container: {
@@ -13,7 +19,28 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   activityContainer: {
+    flex: 12,
+  },
+  programType: {
     flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    width,
+    borderBottomWidth: 1,
+    borderStyle: 'solid',
+    borderBottomColor: 'black',
+  },
+  activityAddButton: {
+    borderWidth: 1,
+    resizeMode: 'cover',
+    width: 70,
+    height: 70,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    right: 20,
+    bottom: 20,
   },
 });
 
@@ -22,25 +49,27 @@ export default function ActivityScreen({ navigation }) {
    * Go ahead and delete ExpoConfigView and replace it with your content;
    * we just wanted to give you a quick view of your config.
    */
+  const activityAdd = require('../assets/images/AddActivity.png');
   const [isActivity, setIsActivity] = useState(true);
   const [type, setType] = useState('');
   return (
     <View style={styles.container}>
-      <Text>활동페이지</Text>
-      <TouchableOpacity
-        onPress={() => {
-          setIsActivity(true);
-        }}
-      >
-        <Text>개인</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => {
-          setIsActivity(false);
-        }}
-      >
-        <Text>서울시</Text>
-      </TouchableOpacity>
+      <View style={styles.programType}>
+        <TouchableOpacity
+          onPress={() => {
+            setIsActivity(true);
+          }}
+        >
+          <NanumGothicBold>개인</NanumGothicBold>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            setIsActivity(false);
+          }}
+        >
+          <NanumGothicBold>서울시</NanumGothicBold>
+        </TouchableOpacity>
+      </View>
       {isActivity ? (
         <View style={styles.activityContainer}>
           <TypePicker type={type} setType={setType} />
@@ -49,11 +78,13 @@ export default function ActivityScreen({ navigation }) {
       ) : (
         <SeoulPrograms />
       )}
-      <TouchableOpacity
-        onPress={() => navigation.navigate('Edit', { id: 'new' })}
-      >
-        <Text>개설하기</Text>
-      </TouchableOpacity>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Edit', { id: 'new' })}
+        >
+          <Image source={activityAdd} style={styles.activityAddButton} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
