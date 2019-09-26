70a4add7f67e3d1cff86927c18bf1f142ed19405
Eunwoo <dmsdn960@gmail.com>
2019-09-27 01:49:28 +0900

    add place description

--- src/components/PlaceDescription/index.jsx
+++ src/components/PlaceDescription/index.jsx
@@ -1,12 +1,38 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
+import Colors from '../../constants/Colors';
+import { font, normalize } from '../../constants/Layout';
+import { NanumGothicExtraBold, NanumGothic } from '../StyledText';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignSelf: 'stretch',
+    paddingHorizontal: normalize(font),
+  },
+  title: {
+    paddingHorizontal: normalize(font / 2),
+    color: Colors.mainColor,
+    fontSize: normalize(font * 1.5),
+    paddingVertical: normalize(font),
+    borderBottomColor: '#e2e2e3',
+    borderBottomWidth: 1,
+  },
+  info: {
+    flexDirection: 'row',
+    paddingVertical: normalize(font),
+  },
+  infoText: {
+    flex: 1,
+    color: '#707070',
+  },
+  infoImage: {
+    width: normalize(font * 1.2),
+    height: normalize(font * 1.2),
+    resizeMode: 'contain',
+    marginHorizontal: normalize(font),
   },
 });
 
@@ -18,10 +44,21 @@ export default function PlaceDescription({
 }) {
   return (
     <View style={styles.container}>
-      <Text>{name}</Text>
-      <Text>{address}</Text>
-      <Text>{contact}</Text>
-      <Text>{businessHour}</Text>
+      <NanumGothicExtraBold style={styles.title}>{name}</NanumGothicExtraBold>
+      <View style={styles.info}>
+        <Image
+          style={styles.infoImage}
+          source={require('./../../assets/images/Places_Gray.png')}
+        />
+        <NanumGothic style={styles.infoText}>{address}</NanumGothic>
+      </View>
+      <View style={styles.info}>
+        <Image
+          style={styles.infoImage}
+          source={require('./../../assets/images/Phone_Gray.png')}
+        />
+        <NanumGothic style={styles.infoText}>{contact}</NanumGothic>
+      </View>
     </View>
   );
 }
