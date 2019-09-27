f73d3804bc4fd45f299a4a9903450f1c3066a3df
Eunwoo <dmsdn960@gmail.com>
2019-09-27 21:29:06 +0900

    change logo image and add scheme

--- src/components/SeoulPrograms/index.jsx
+++ src/components/SeoulPrograms/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList, Text } from 'react-native';
+import { FlatList } from 'react-native';
 import { useQuery } from '@apollo/react-hooks';
 import Program from './Program';
 import GET_PROGRAMS from './query';
